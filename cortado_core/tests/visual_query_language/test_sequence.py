fd956c996e52c27f8297c88c8a1e40e13db779b9
Tobias Bungard <tobi-bungard@t-online.de>
2025-11-25 18:06:45 +0100

    Added complex sequence test for visual query language

--- cortado_core/tests/visual_query_language/test_sequence.py
+++ cortado_core/tests/visual_query_language/test_sequence.py
@@ -1,5 +1,11 @@
 import unittest
-from cortado_core.utils.split_graph import SequenceGroup, ParallelGroup, LeafGroup
+from cortado_core.utils.split_graph import (
+    SequenceGroup,
+    ParallelGroup,
+    LeafGroup,
+    StartGroup,
+    EndGroup,
+)
 from cortado_core.visual_query_language.query import check_variant
 
 
@@ -126,3 +132,94 @@ class SimpleSequenceTest(unittest.TestCase):
         )
 
         self.assertFalse(check_variant(variant, self.query, self.activities))
+
+
+class PrefixSuffixTest(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.query = SequenceGroup(
+            lst=[
+                LeafGroup(lst=["a"]),
+                LeafGroup(lst=["b"]),
+                LeafGroup(lst=["c"]),
+                ParallelGroup(lst=[LeafGroup(lst=["d"]), LeafGroup(lst=["e"])]),
+                LeafGroup(lst=["a"]),
+                LeafGroup(lst=["b"]),
+                LeafGroup(lst=["c"]),
+            ],
+        )
+
+    def test_prefix(self):
+        variant = SequenceGroup(
+            lst=[
+                LeafGroup(lst=["a"]),
+                LeafGroup(lst=["b"]),
+                LeafGroup(lst=["c"]),
+                ParallelGroup(lst=[LeafGroup(lst=["d"]), LeafGroup(lst=["e"])]),
+                LeafGroup(lst=["a"]),
+                LeafGroup(lst=["b"]),
+                LeafGroup(lst=["c"]),
+                ParallelGroup(lst=[LeafGroup(lst=["x"]), LeafGroup(lst=["y"])]),
+                LeafGroup(lst=["a"]),
+                LeafGroup(lst=["b"]),
+                LeafGroup(lst=["c"]),
+            ]
+        )
+
+        activities = [chr(i) for i in range(ord("a"), ord("z") + 1)]
+        self.assertTrue(check_variant(variant, self.query, activities))
+
+    def test_suffix(self):
+        variant = SequenceGroup(
+            lst=[
+                LeafGroup(lst=["a"]),
+                LeafGroup(lst=["b"]),
+                LeafGroup(lst=["c"]),
+                ParallelGroup(lst=[LeafGroup(lst=["x"]), LeafGroup(lst=["y"])]),
+                LeafGroup(lst=["a"]),
+                LeafGroup(lst=["b"]),
+                LeafGroup(lst=["c"]),
+                ParallelGroup(lst=[LeafGroup(lst=["d"]), LeafGroup(lst=["e"])]),
+                LeafGroup(lst=["a"]),
+                LeafGroup(lst=["b"]),
+                LeafGroup(lst=["c"]),
+            ]
+        )
+
+        activities = [chr(i) for i in range(ord("a"), ord("z") + 1)]
+        self.assertTrue(check_variant(variant, self.query, activities))
+
+    def test_complete(self):
+        query = SequenceGroup(
+            lst=[
+                StartGroup(),
+                LeafGroup(lst=["a"]),
+                LeafGroup(lst=["b"]),
+                LeafGroup(lst=["c"]),
+                ParallelGroup(lst=[LeafGroup(lst=["d"]), LeafGroup(lst=["e"])]),
+                LeafGroup(lst=["a"]),
+                LeafGroup(lst=["b"]),
+                LeafGroup(lst=["c"]),
+                EndGroup(),
+            ],
+        )
+
+        variant = SequenceGroup(
+            lst=[
+                LeafGroup(lst=["a"]),
+                LeafGroup(lst=["b"]),
+                LeafGroup(lst=["c"]),
+                ParallelGroup(lst=[LeafGroup(lst=["x"]), LeafGroup(lst=["y"])]),
+                LeafGroup(lst=["a"]),
+                LeafGroup(lst=["b"]),
+                LeafGroup(lst=["c"]),
+                ParallelGroup(lst=[LeafGroup(lst=["d"]), LeafGroup(lst=["e"])]),
+                LeafGroup(lst=["a"]),
+                LeafGroup(lst=["b"]),
+                LeafGroup(lst=["c"]),
+            ]
+        )
+
+        activities = [chr(i) for i in range(ord("a"), ord("z") + 1)]
+        self.assertFalse(check_variant(variant, self.query, activities))
