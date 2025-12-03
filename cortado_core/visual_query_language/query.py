5a5eb6c6d2e81e017f059909461e50a5fb5adfdb
jub <julius.brehme@googlemail.com>
2025-12-03 11:34:19 +0100

    added starting point of query algorithm

--- cortado_core/visual_query_language/query.py
+++ cortado_core/visual_query_language/query.py
@@ -1,10 +1,30 @@
+from typing import List
+
 from cortado_core.utils.split_graph import SequenceGroup
 from cortado_core.visual_query_language.matching_algorithm import match_sequential
-from typing import List
+from cortado_core.visual_query_language.unfold_tree import unfold_tree
+
+
+def start_query(variant: SequenceGroup, query: SequenceGroup) -> bool:
+    """
+    Starting point of the query.
+     Args:
+        variant (Group): The variant to be checked.
+        query (Group): The query pattern.
+    First unfolds the tree and then checks the variants.
+    Returns:
+        bool: True if one variant matches the query, False otherwise.
+    """
+
+    unfolded_tree_list = unfold_tree(variant)
+    for variant_tree in unfolded_tree_list:
+        if check_variant(variant_tree, query):
+            return True
+    return False
 
 
 def check_variant(
-    variant: SequenceGroup, query: SequenceGroup, activities: List[str]
+    variant: SequenceGroup, query: SequenceGroup, activities: List[str] = []
 ) -> bool:
     """
     Check if the given variant matches the query pattern.
